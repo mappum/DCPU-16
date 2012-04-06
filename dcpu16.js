0a74230de70380e66fe9966e9c07a146abdffad9
Matt Bell <matt@safeb.it>
2012-04-06 03:18:20 -0700

    Added MIT license to code

--- dcpu16.js
+++ dcpu16.js
@@ -1,9 +1,12 @@
 /*
- * DCPU-16 Emulation
- * By Matt Bell (mappum)
- */
+Copyright (c) 2012 Matt Bell
 
-/*jslint devel: true, browser: true, bitwise: true, unparam: true, plusplus: true, maxerr: 50, indent: 4, nomen: true */
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
 
 // namespace
 var DCPU16 = {};
@@ -670,7 +673,7 @@ DCPU16.Assembler = (function () {
 
 				//next word + register
 				if (arg.split('+').length === 2
-						&& (parseInt(arg.split('+')[0], 16) || parseLiteral(arg.split('+')[0]) === 0)
+						&& (parseLiteral(arg.split('+')[0], 16) || parseLiteral(arg.split('+')[0]) === 0)
 						&& typeof arg.split('+')[1] === 'string'
 						&& typeof cpu.mem[arg.split('+')[1].toLowerCase()] === 'number') {
 
