878634dca36bf41df77c166847dc34c23000ba8c
Jonathan Castello <twisolar@gmail.com>
2012-04-12 02:14:45 -0700

    Set up a couple skeleton testing files.

---
+++ test/assembler_test.js
@@ -0,0 +1,7 @@
+var Assembler = require("../dcpu16.js").Assembler;
+var assert = require("assert");
+
+module.exports = {
+    'dummy test': function() {
+    }
+};
---
+++ test/cpu_test.js
@@ -0,0 +1,7 @@
+var CPU = require("../dcpu16.js").CPU;
+var assert = require("assert");
+
+module.exports = {
+    'dummy test': function() {
+    }
+};
