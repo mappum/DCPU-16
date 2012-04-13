0c5b8dfb864a1ba9a1e0c2031828e8ae49734e74
Jonathan Castello <twisolar@gmail.com>
2012-04-12 23:08:11 -0700

    Use .get() and .set() instead of .mem[] in the tests.

--- test/cpu_test.js
+++ test/cpu_test.js
@@ -24,10 +24,10 @@ module.exports = {
         var cpu = new CPU();
 
         // SET [next word], next word
-        cpu.mem[0x0000] = 0x7DE1;
-        cpu.mem[0x0001] = 0x4242;
-        cpu.mem[0x0002] = 0xC0C0;
-        cpu.mem['pc'] = 0x0000;
+        cpu.set(0x0000, 0x7DE1);
+        cpu.set(0x0001, 0x4242);
+        cpu.set(0x0002, 0xC0C0);
+        cpu.set('pc', 0x0000);
 
         var insn = cpu.nextInstruction();
         assert.equal(insn.opcode, 0);
@@ -35,8 +35,8 @@ module.exports = {
         assert.equal(insn.bAddr, 0x0002);
 
         // BRK
-        cpu.mem[0x0000] = 0x0020;
-        cpu.mem['pc'] = 0x0000;
+        cpu.set(0x0000, 0x0020);
+        cpu.set('pc', 0x0000);
         var insn = cpu.nextInstruction();
         assert.equal(insn.opcode, 17);
     },
@@ -45,27 +45,27 @@ module.exports = {
         var cpu = new CPU();
 
         // SET A, B
-        cpu.mem[0x0000] = 0x0401;
-        cpu.mem['pc'] = 0x0000;
+        cpu.set(0x0000, 0x0401);
+        cpu.set('pc', 0x0000);
         cpu.nextInstruction();
-        assert.equal(cpu.mem['pc'], 0x0001);
+        assert.equal(cpu.get('pc'), 0x0001);
 
         // SET A, next word
-        cpu.mem[0x0000] = 0x7C01;
-        cpu.mem['pc'] = 0x0000;
+        cpu.set(0x0000, 0x7C01);
+        cpu.set('pc', 0x0000);
         cpu.nextInstruction();
-        assert.equal(cpu.mem['pc'], 0x0002);
+        assert.equal(cpu.get('pc'), 0x0002);
 
         // SET next word, A
-        cpu.mem[0x0000] = 0x01F1;
-        cpu.mem['pc'] = 0x0000;
+        cpu.set(0x0000, 0x01F1);
+        cpu.set('pc', 0x0000);
         cpu.nextInstruction();
-        assert.equal(cpu.mem['pc'], 0x0002);
+        assert.equal(cpu.get('pc'), 0x0002);
 
         // SET [next word], [next word]
-        cpu.mem[0x0000] = 0x79E1;
-        cpu.mem['pc'] = 0x0000;
+        cpu.set(0x0000, 0x79E1);
+        cpu.set('pc', 0x0000);
         cpu.nextInstruction();
-        assert.equal(cpu.mem['pc'], 0x0003);
+        assert.equal(cpu.get('pc'), 0x0003);
     }
 };
