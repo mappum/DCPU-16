3021e850f86cae13da7b322d554754f8f681b9c1
Jonathan <twisolar@gmail.com>
2012-04-15 02:14:28 -0700

    Filled out the CPU#addressFor() test suite.

--- test/cpu_test.js
+++ test/cpu_test.js
@@ -4,20 +4,48 @@ var assert = require("assert");
 module.exports = {
     'test CPU#addressFor': function() {
         var cpu = new CPU();
-        var registers = ["a", "b", "c", "x", "y", "z", "i", "j"];
 
+        // Simple register-based values
+        var registers = ["a", "b", "c", "x", "y", "z", "i", "j"];
         cpu.set(0x0000, 4);
         for (var i = 0, _len = registers.length; i < _len; ++i) {
             cpu.set("pc", 0x0000);
             cpu.set(registers[i], 0xF0F0 + i);
             cpu.set(0xF0F0 + i, 42);
 
-            assert(cpu.addressFor(i), registers[i]);
-            assert(cpu.addressFor(i+8), 0xF0F0 + i);
-            assert(cpu.addressFor(i+16), 42);
+            assert.equal(cpu.addressFor(i), registers[i]);
+            assert.equal(cpu.addressFor(i+8), 0xF0F0 + i);
+            assert.equal(cpu.addressFor(i+16), 0xF0F0 + i + 4);
         }
 
-        // TODO: rest of the value types
+        assert.equal(cpu.addressFor(0x1B), "stack");
+        assert.equal(cpu.addressFor(0x1C), "pc");
+        assert.equal(cpu.addressFor(0x1D), "o");
+
+        // Encoded literals
+        for (var i = 0; i < 32; ++i) {
+            assert.equal(cpu.addressFor(0x20 + i), i);
+        }
+
+        // Stack operations
+        cpu.set("stack", 0xFFFF);
+        assert.equal(cpu.addressFor(0x18), 0xFFFF);
+        assert.equal(cpu.get("stack"), 0x0000);
+
+        cpu.set("stack", 0xFFFF);
+        assert.equal(cpu.addressFor(0x19), 0xFFFF);
+        assert.equal(cpu.get("stack"), 0xFFFF);
+
+        cpu.set("stack", 0xFFFF);
+        assert.equal(cpu.addressFor(0x1A), 0xFFFE);
+        assert.equal(cpu.get("stack"), 0xFFFE);
+
+        // next-word value types
+        cpu.set(0x0000, 0x4242);
+        cpu.set("pc", 0x0000);
+        assert.equal(cpu.addressFor(0x1E), 0x4242); // used as address
+        cpu.set("pc", 0x0000);
+        assert.equal(cpu.addressFor(0x1F), 0x4242); // used as literal
     },
 
     'test CPU#nextInstruction': function() {
@@ -69,6 +97,7 @@ module.exports = {
         assert.equal(cpu.get('pc'), 0x0003);
     },
 
+    // literal value-types must not be assignable
     'test unassignable literals': function() {
         var cpu = new CPU();
 
@@ -84,7 +113,7 @@ module.exports = {
             assert.equal(cpu.get(i+1), 0x4242);
         }
 
-        // trailing literals
+        // next-word literal
         cpu.set('pc', 0x0000);
         cpu.set('a', 0x1234);
 
@@ -95,6 +124,7 @@ module.exports = {
         assert.equal(cpu.get(0x0001), 0x4242);
     },
 
+    // [A+0xFFFF] should have the same effect as [A-1]
     'test offset wraparound': function() {
         var cpu = new CPU();
 
