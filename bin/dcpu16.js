418a4c8fec621adbc915037c4393c6601978a7cd
Matt Bell <matt@safeb.it>
2012-04-26 22:55:50 -0700

    Updated for node assembler

--- bin/dcpu16.js
+++ bin/dcpu16.js
@@ -1,17 +1,48 @@
 #!/usr/bin/env node
-var DCPU = require('../index.js');
 
-var cpu = new DCPU.CPU();
-var assembler = new DCPU.Assembler(cpu);
+var dcpu = require('../index.js');
+
+var cpu = new dcpu.CPU();
+var assembler = new dcpu.Assembler(cpu);
+
+var hex = process.argv.indexOf('-h') !== -1;
+var fullSize = process.argv.indexOf('-f') !== -1;
 
 var data = '';
 process.stdin.setEncoding('utf8');
 process.stdin.on('data', function(chunk){ data += chunk; });
 process.stdin.on('end', function(){
-    assembler.compile(data);
-    for(var i = 0; i < assembler.addressMap.length - 1; i++) {
-        process.stdout.write(cpu.mem[i].toString(16) + " ");
+	try {
+	    assembler.compile(data);
+	    
+	    var lastNonzero;
+	    if(!fullSize) {
+		    for(var i = cpu.ramSize - 1; i >= 0; i--) {
+		    	if(cpu.mem[i]) {
+		    		lastNonzero = i;
+		    		break;
+		    	}
+		    }
+	    } else {
+	    	lastNonzero = cpu.ramSize - 1;
+	    }
+	    
+	    if(hex) {
+	    	var string = '';
+	    	for(var i = 0; i <= lastNonzero; i++) {
+	    		string += dcpu.CPU.formatWord(cpu.mem[i]);
+	    	}
+	    	process.stdout.write(string + '\n');
+	    } else {
+	    	
+	    	var buffer = new Buffer(lastNonzero + 1);
+	    	for(var i = 0; i <= lastNonzero; i++) {
+	    		buffer[i] = cpu.mem[i];
+	    	}
+	    	process.stdout.write(buffer.toString('ucs2') + '\n');
+	    }
+    } catch(e) {
+    	console.error(e);
     }
-    process.stdout.write("\n");
 });
 process.stdin.resume();
