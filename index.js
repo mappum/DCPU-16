fd2675c19bf085148080d691d11275f12729db9b
Matt Bell <matt@safeb.it>
2012-04-27 02:46:05 -0700

    Fixed drive device for node

--- index.js
+++ index.js
@@ -2,5 +2,7 @@
 
 module.exports = {
     CPU: require("./lib/cpu.js"),
-    Assembler: require("./lib/assembler.js")
+    Assembler: require("./lib/assembler.js"),
+    HMD2043: require("./lib/HMD2043.js"),
+    HMU1440: require("./lib/HMU1440.js")
 };
