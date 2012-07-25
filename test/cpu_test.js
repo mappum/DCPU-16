44f356c40ca6e7aa7014842c0ed2481f62495be9
Nicklas Lindgren <nili@gulmohar.se>
2012-07-25 21:19:50 +0200

    Update test suite, fix minor issue with [sp++] addressing

--- test/cpu_test.js
+++ test/cpu_test.js
@@ -11,34 +11,43 @@ module.exports = {
         for (var i = 0, _len = registers.length; i < _len; ++i) {
             cpu.set("pc", 0x0000);
             cpu.set(registers[i], 0xF0F0 + i);
-            cpu.set(0xF0F0 + i, 42);
 
             assert.equal(cpu.addressFor(i), registers[i]);
             assert.equal(cpu.addressFor(i+8), 0xF0F0 + i);
             assert.equal(cpu.addressFor(i+16), 0xF0F0 + i + 4);
         }
 
-        assert.equal(cpu.addressFor(0x1B), "stack");
+        assert.equal(cpu.addressFor(0x1B), "sp");
         assert.equal(cpu.addressFor(0x1C), "pc");
         assert.equal(cpu.addressFor(0x1D), "ex");
 
         // Encoded literals
-        for (var i = -1; i < 31; ++i) {
+        for (i = -1; i < 31; ++i) {
             assert.equal(cpu.addressFor(0x21 + i), (i & 0xffff) | CPU.FLAG_LITERAL);
         }
 
         // Stack operations
-        cpu.set("stack", 0xFFFF);
-        assert.equal(cpu.addressFor(0x18), 0xFFFF);
-        assert.equal(cpu.get("stack"), 0x0000);
+        // Push
+        cpu.set("sp", 0xFFFF);
+        assert.equal(cpu.addressFor(0x18, false), 0xFFFE);
+        assert.equal(cpu.get("sp"), 0xFFFE);
 
-        cpu.set("stack", 0xFFFF);
+        // Pop
+        cpu.set("sp", 0xFFFF);
+        assert.equal(cpu.addressFor(0x18, true), 0xFFFF);
+        assert.equal(cpu.get("sp"), 0x0000);
+
+        // Peek
+        cpu.set("sp", 0xFFFF);
         assert.equal(cpu.addressFor(0x19), 0xFFFF);
-        assert.equal(cpu.get("stack"), 0xFFFF);
+        assert.equal(cpu.get("sp"), 0xFFFF);
 
-        cpu.set("stack", 0xFFFF);
+        // Pick
+        cpu.set("sp", 0xFFFF);
+        cpu.set(0x0000, 0xFFFF);
+        cpu.set("pc", 0x0000);
         assert.equal(cpu.addressFor(0x1A), 0xFFFE);
-        assert.equal(cpu.get("stack"), 0xFFFE);
+        assert.equal(cpu.get("sp"), 0xFFFF);
 
         // next-word value types
         cpu.set(0x0000, 0x4242);
@@ -68,21 +77,21 @@ module.exports = {
         var cpu = new CPU();
 
         // SET [next word], next word
-        cpu.set(0x0000, 0x7DE1);
+        cpu.set(0x0000, 0x7FC1);
         cpu.set(0x0001, 0x4242);
         cpu.set(0x0002, 0xC0C0);
         cpu.set('pc', 0x0000);
 
         var insn = cpu.nextInstruction();
-        assert.equal(insn.opcode, 0);
-        assert.equal(insn.a, 0x4242);
-        assert.equal(insn.b, 0xC0C0 | CPU.FLAG_LITERAL);
+        assert.equal(insn.opcode, 1);
+        assert.equal(insn.a, 0x4242 | CPU.FLAG_LITERAL);
+        assert.equal(insn.b, 0xC0C0);
 
         // BRK
         cpu.set(0x0000, 0x0020);
         cpu.set('pc', 0x0000);
-        var insn = cpu.nextInstruction();
-        assert.equal(insn.opcode, 17);
+        insn = cpu.nextInstruction();
+        assert.equal(insn.opcode, 32);
     },
 
     'test instruction length': function() {
@@ -101,13 +110,13 @@ module.exports = {
         assert.equal(cpu.get('pc'), 0x0002);
 
         // SET next word, A
-        cpu.set(0x0000, 0x01F1);
+        cpu.set(0x0000, 0x03E1);
         cpu.set('pc', 0x0000);
         cpu.nextInstruction();
         assert.equal(cpu.get('pc'), 0x0002);
 
         // SET [next word], [next word]
-        cpu.set(0x0000, 0x79E1);
+        cpu.set(0x0000, 0x7BC1);
         cpu.set('pc', 0x0000);
         cpu.nextInstruction();
         assert.equal(cpu.get('pc'), 0x0003);
@@ -149,9 +158,9 @@ module.exports = {
         cpu.set(0x0042, 0x1234);
 
         // SET [A+0xFFFF] 0x4321;
-        cpu.set(0x0000, 0x7D01);
-        cpu.set(0x0001, 0xFFFF);
-        cpu.set(0x0002, 0x4321);
+        cpu.set(0x0000, 0x7E01);
+        cpu.set(0x0001, 0x4321);
+        cpu.set(0x0002, 0xFFFF);
         cpu.step();
         assert.equal(cpu.get(0x0042), 0x1234);
         assert.equal(cpu.get(0x0041), 0x4321);
